66415aed820f5fe81755e7d81307901537ab5d56
Sarah Walker <sarahsweetpie6@gmail.com>
2025-04-01 09:52:47 -0600

    added code from textbook to give an outline

--- Recommendations/obj/Debug/net8.0/Recommendations.AssemblyInfo.cs
+++ Recommendations/obj/Debug/net8.0/Recommendations.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recommendations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58bfde4e4d6aec55cdb37a18b3b5e95ba8ebb31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recommendations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recommendations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
